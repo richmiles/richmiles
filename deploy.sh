ba3d084aae943c9ec431901bdf1efea2fa6f88e3
Rich Miles <me@richmiles.xyz>
2023-01-11 10:34:19 -0700

    Added code to build the richmiles repo and copy it to richmiles.github.io repo.

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 npm install
 npm run build
+cd ..
+rm -rf richmiles.github.io/
 git clone https://github.com/richmiles/richmiles.github.io.git
-rm -rf other-repo/*
-cp -r dist/* other-repo/
-cd other-repo
+rm -rf richmiles.github.io/*
+cp -r richmiles/dist/* richmiles.github.io/
+cd richmiles.github.io
 git add .
 git commit -m "Publishing new build"
 git push
