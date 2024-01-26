7575d2df584a2278f219daf403b05a3c0ce72c9c
Rich Miles <me@richmiles.xyz>
2024-01-26 11:51:44 -0700

    Troubleshooting push.

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ npm run build
 # Clone the target repository
 TARGET_REPO_DIR="other-repo"
 
-echo "Cloning target repo..."
+echo "Cloning target repo ${TARGET_REPO_URL}"
 git clone https://${ACCESS_TOKEN}@${TARGET_REPO_URL} $TARGET_REPO_DIR
 
 # Copy over the artifacts
@@ -18,6 +18,8 @@ cp -r dist/* $TARGET_REPO_DIR/
 
 # Commit and push
 cd $TARGET_REPO_DIR
+
+echo "Committing and pushing to ${TARGET_REPO_URL}"
 git set remote origin https://${ACCESS_TOKEN}@${TARGET_REPO_URL}
 
 git config user.name "GitHub Actions"
