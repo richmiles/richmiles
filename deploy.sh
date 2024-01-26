91da323da8c652f7e80ebc7917e2ded970e6815a
Rich Miles <me@richmiles.xyz>
2024-01-26 12:25:39 -0700

    Fixed issue with target repository variable.

--- deploy.sh
+++ deploy.sh
@@ -12,19 +12,18 @@ TARGET_REPO_DIR="other-repo"
 echo "Cloning target repo ${TARGET_REPO_URL} into ${TARGET_REPO_DIR}"
 git clone https://${ACCESS_TOKEN}@${TARGET_REPO_URL} $TARGET_REPO_DIR
 
-# # Copy over the artifacts
-# echo "Copying artifacts from dist to ${TARGET_REPO_DIR}"
-# rsync -av --exclude='.git' --delete dist/ $TARGET_REPO_DIR/
+# Copy over the artifacts
+echo "Copying artifacts from dist to ${TARGET_REPO_DIR}"
+rsync -av --exclude='.git' --delete dist/ $TARGET_REPO_DIR/
 
-# # Commit and push
-# cd $TARGET_REPO_DIR
-# ls -la
+# Commit and push
+cd $TARGET_REPO_DIR
 
-# echo "Committing and pushing to ${TARGET_REPO_URL}"
-# git set remote origin https://${ACCESS_TOKEN}@${TARGET_REPO_URL}
+echo "Committing and pushing to ${TARGET_REPO_URL}"
+git set remote origin https://${ACCESS_TOKEN}@${TARGET_REPO_URL}
 
-# git config user.name "GitHub Actions"
-# git config user.email "actions@github.com"
-# git add .
-# git commit -m "Publishing new build"
-# git push
+git config user.name "GitHub Actions"
+git config user.email "actions@github.com"
+git add .
+git commit -m "Publishing new build"
+git push
