b63e712a657c4114c418a772a4e829f89d69cd3b
Rich Miles <me@richmiles.xyz>
2024-01-26 11:48:16 -0700

    Setting remote origin.

--- deploy.sh
+++ deploy.sh
@@ -6,23 +6,20 @@ set -e
 npm install
 npm run build
 
-echo "Current Directory:"
-pwd
-echo "Directory Contents:"
-ls -la
-
 # Clone the target repository
 TARGET_REPO_DIR="other-repo"
 
 echo "Cloning target repo..."
 git clone https://${ACCESS_TOKEN}@${TARGET_REPO_URL} $TARGET_REPO_DIR
 
+# Copy over the artifacts
 rm -rf $TARGET_REPO_DIR/*
 cp -r dist/* $TARGET_REPO_DIR/
 
-
 # Commit and push
 cd $TARGET_REPO_DIR
+git set remote origin https://${ACCESS_TOKEN}@${TARGET_REPO_URL}
+
 git config user.name "GitHub Actions"
 git config user.email "actions@github.com"
 git add .
